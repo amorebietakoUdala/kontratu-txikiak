f536d70262e453c8e3d15879b3b68cb58115d303
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2023-12-20 14:21:58 +0100

    Updated to SF6.4

--- src/Controller/ContractController.php
+++ src/Controller/ContractController.php
@@ -15,7 +15,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Translation\TranslatableMessage;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use \Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
